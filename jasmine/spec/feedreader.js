c7f6c923121bc5f914c2a3b3d0f29db9a8c259e4
Gustavo Trevisan Dini <gustavotrevisandini@gmail.com>
2018-07-21 01:53:12 -0300

    Ready for Submit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,10 +31,12 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         // Este Teste é Bem Direto, ele apenas verificará com um loop for se todos
+         // os feed presentes no JSON tem uma URL e ela não é nula
          it('have URL', function(){
            for (feed in allFeeds){
              expect(allFeeds[feed].url).toBeDefined();
-             expect(allFeeds[feed].url).not.toBe("");
+             expect(allFeeds[feed].url).not.toBeNull();
            }
          });
 
@@ -43,10 +45,12 @@ $(function() {
          * and that the name is not empty.
          */
 
+         // Como no teste anterior, ele  verificará com um loop for se todos
+         // os feed presentes no JSON tem uma Nome e não é nulo
          it('have Name', function(){
            for (feed in allFeeds){
              expect(allFeeds[feed].name).toBeDefined();
-             expect(allFeeds[feed].name).not.toBe("");
+             expect(allFeeds[feed].name).not.toBeNull();
            }
          });
 
@@ -55,14 +59,21 @@ $(function() {
     /* TODO: Write a new test suite named "The menu" */
 
     describe('The menu', function() {
+      // Aqui defini as variaveis que irei usar nos proximos testes.
+      // criei uma variavel para o botáo de menu,
+      let menuIcon = $('.menu-icon-link');
+      // e criei uma variavel booleana para ver se inicialmente o body tem a classe "menu-hidden"
+      let initialHiddenValue = $('body').hasClass("menu-hidden");
 
       /* TODO: Write a test that ensures the menu element is
        * hidden by default. You'll have to analyze the HTML and
        * the CSS to determine how we're performing the
        * hiding/showing of the menu element.
        */
+
+       // Como já defini anteriormente, neste teste apenas verifiquei se initialHiddenValue é verdadeira
        it('is Hidden', function(){
-         expect($("body").hasClass("menu-hidden")).toBe(true);
+         expect(initialHiddenValue).toBe(true);
        });
 
        /* TODO: Write a test that ensures the menu changes
@@ -70,6 +81,16 @@ $(function() {
         * should have two expectations: does the menu display when
         * clicked and does it hide when clicked again.
         */
+
+        // Aqui fiz 2 simulações do click do menuIcon e a cada click comparei o valor de initialHiddenValue
+        // com o valor que procurei na verificação da classe "menu-hidden", a logica é com um click ser diferente
+        // e com o proximo click retornar ao valor inicial
+        it('toggle Visibility', function(){
+          menuIcon.click();
+          expect($("body").hasClass("menu-hidden")).not.toBe(initialHiddenValue);
+          menuIcon.click();
+          expect($("body").hasClass("menu-hidden")).toBe(initialHiddenValue);
+        });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
@@ -83,6 +104,26 @@ $(function() {
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
 
+       // Aqui defini as variaveis que irei usar nos proximos testes.
+       // criei uma variavel para armazenar o tamanho do feed
+       let feedSize;
+       // e armazenei em uma variavel o feedlist
+       let feedList = $('.feed-list');
+
+       // acessei o loadFeed assincrona e verifiquei o tamanho da lista retornada e armazenei em feedSize
+       beforeEach(function(done){
+         loadFeed(0, () => {
+           feedSize = feedList.length;
+           done();
+         })
+       });
+
+       // aqui verifiquei se o valor de feedSize é maior que 0
+       it('should load initial feeds', function(done){
+         expect(feedSize).toBeGreaterThan(0);
+         done();
+       });
+
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
@@ -94,7 +135,33 @@ $(function() {
        * Remember, loadFeed() is asynchronous.
        */
 
+       // Aqui defini as variaveis que irei usar nos proximos testes.
+       // criei duas variaveis para armazenar cada feed
+       let feedA;
+       let feedB;
+       // e armazenei em uma variavel o feed
+       let feedContainer = $('.feed');
+
+       // acessei o loadFeed assincrona 2 vezes com valores de feed diferentes
+       // e armazenei o html do feed em cada variavel feedA e feedB
+       beforeEach(function(done){
+         loadFeed(0, () => {
+           feedA = feedContainer.html();
+           console.log(feedA);
+         });
+
+         loadFeed(1, () => {
+           feedB = feedContainer.html();
+           console.log(feedB);
+           done();
+         });
+
+       });
+
+       // comparei as duas variaveis, o teste passa apenas se as duas forem diferentes
+       it('should change feeds on loadFeed()', function(done){
+         expect(feedA).not.toMatch(feedB);
+         done();
+       });
     });
-
-
 }());
